705abdd397e5743dd6094b555708cc75e60f9566
Xavatt <xavierguinto3@outlook.es>
2018-09-13 19:09:44 -0500

    Some Final Comments plus Format Document

--- Process.h
+++ Process.h
@@ -13,7 +13,7 @@
  *
  */
 
-//We make a typedef to facilitate declaration of process_p structures in the future
+/* We make a typedef to facilitate declaration of process_p structures in the future */
 typedef struct process_p *Process;
 
 /* Declaration of the data structure process_p that will hold information about
@@ -21,29 +21,29 @@ typedef struct process_p *Process;
   */
 struct process_p
 {
-    int process_id;              //!< The id of the process
-    int process_arrival;         //!< The arrival time of the process
-    int process_burst;           //!< The cpu burst of the process
-    int process_priority;        //!< The priority of the process
-    int process_runtime;         //!< The total time the process has run
-    int process_lastruntime;     //!< The last time the process started execution
-    int process_remainingcycles; //!< The cpu burst left in the process
+  int process_id;              /* The id of the process */
+  int process_arrival;         /* The arrival time of the process */
+  int process_burst;           /* The cpu burst of the process */
+  int process_priority;        /* The priority of the process */
+  int process_runtime;         /* The total time the process has run */
+  int process_lastruntime;     /* The last time the process started execution */
+  int process_remainingcycles; /* The cpu burst left in the process */
 };
 
-/*! We declare an enum to facilitate the use of
-  *  sort types and algorithm types we may use
-  */
+/* We declare an enum to facilitate the use of
+*  sort types and algorithm types we may use */
 enum sort_type
 {
-    ID //!< Constant for ID
-    ,
-    ARRIVAL //!< Constant for Arrival
-    ,
-    PRIORITY //!< Constant for Priority
-    ,
-    CPUBURST //!< Constant for Cpu burst
+  ID /* Constant for ID */
+  ,
+  ARRIVAL /* Constant for Arrival */
+  ,
+  PRIORITY /* Constant for Priority */
+  ,
+  CPUBURST /* Constant for Cpu burst */
 };
-// Consult documentation or Process.c for more information.
+
+/* Consult documentation or Process.c for more information. */
 GList *CreateProcess(GList *process_list, int id, int arrival, int burst, int priority, int algo);
 
 GList *SortProcessList(GList *process_list, int sort);
